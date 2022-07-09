940a40466e73598226a3ea11866db8f0c49697f3
Ronald <106852240+RonaldDelgadoA@users.noreply.github.com>
2022-07-09 17:26:34 -0500

    Add files via upload

---
+++ PatronDecorator/Crema.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronDecorator
+{
+    public class Crema : AgregadoDecorator
+    {
+        public Crema(BebidaComponent bebida) : base(bebida) { }
+        public override double Costo => _bebida.Costo + 4;
+        public override string Descripcion => string.Format($"{_bebida.Descripcion}, Crema");
+
+    }
+
+}
